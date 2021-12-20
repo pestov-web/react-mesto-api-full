c5d5d3716eb3616c52e992190fc453088a63578d
vladimir pestov <pestov.web@yandex.ru>
2021-12-20 16:21:11 +1000

    fix / login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -131,6 +131,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: true,
+        domain: 'pestov.students.nomoredomains.rocks',
       })
         .send({ email: user.email });
     })
