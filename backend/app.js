2530c1e7edd5d41c58787a01040282a729146500
vladimir pestov <pestov.web@yandex.ru>
2021-12-20 18:58:06 +1000

    fix / login

--- backend/app.js
+++ backend/app.js
@@ -21,11 +21,25 @@ const auth = require('./middlewares/auth');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors({
-  'credentials': true,
-  'origin': '*',
-  exposedHeaders: ["set-cookie"],
-}));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type,set-cookie');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+// app.use(cors({
+//   'credentials': true,
+//   'origin': '*',
+//   exposedHeaders: ["set-cookie"],
+// }));
 
 app.use(helmet());
 app.use(cookieParser());
