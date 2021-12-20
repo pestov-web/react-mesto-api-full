2279401e268033fd587b380238a110e8883c382a
vladimir pestov <pestov.web@yandex.ru>
2021-12-20 18:52:42 +1000

    fix / login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -129,7 +129,7 @@ module.exports.login = (req, res, next) => {
       );
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: "none",
         secure: true,
       })
